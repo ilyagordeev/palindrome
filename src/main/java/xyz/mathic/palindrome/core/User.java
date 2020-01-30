e97f54f9c6d51d51572cd2692adcf38c9c6454c1
Ilya Gordeev <ilya.tanat@gmail.com>
2020-01-30 11:36:43 +0500

    override hashCode & equals

--- src/main/java/xyz/mathic/palindrome/core/User.java
+++ src/main/java/xyz/mathic/palindrome/core/User.java
@@ -1,6 +1,7 @@
 package xyz.mathic.palindrome.core;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 /**
@@ -40,4 +41,16 @@ public class User {
         return points;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return name.equals(user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 }
